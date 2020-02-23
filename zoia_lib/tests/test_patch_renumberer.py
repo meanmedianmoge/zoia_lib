6615296e317b73478e06758e74a09cb1e301ea33
Alex Myers <qckpckt@gmail.com>
2020-02-23 13:36:30 -0800

    adds some error classes and refactors things to make pathing better (maybe)?

--- zoia_lib/tests/test_patch_renumberer.py
+++ zoia_lib/tests/test_patch_renumberer.py
@@ -5,8 +5,10 @@ import unittest
 from zoia_lib.patch_renumberer import Renumber
 
 # Renumber changes the pwd so we save where we started here.
-START_PATH = os.getcwd()
-TEST_PATH = 'zoia_lib/tests/test_files/'
+THIS_DIR = os.path.dirname(__file__)
+TEST_RELATIVE_PATH = 'test_files/'
+RENUMBER_RELATIVE_PATH = 'zoia_lib/tests/test_files/'
+PATH = os.path.join(THIS_DIR, TEST_RELATIVE_PATH)
 TEST_FILE_NAMES = [
     '018_zoia_Afterneath_V4.bin',
     '057_zoia_Fading_Dream1_1.bin'
@@ -16,24 +18,17 @@ TEST_FILE_NAMES = [
 class TestRenumber(unittest.TestCase):
 
     def setUp(self):
-        os.chdir(START_PATH)
-        # save starting directory
-        # create dummy files
-        [open(f'{TEST_PATH}{f}', 'a').close() for f in TEST_FILE_NAMES]
+        [open(os.path.join(PATH, f), 'a').close() for f in TEST_FILE_NAMES]
 
     def tearDown(self):
-        # reset directory
-        os.chdir(START_PATH)
-        os.chdir(TEST_PATH)
-        # remove renamed files -- this is pretty janky if the path isn't right!
-        [os.remove(f)for f in os.listdir()]
+        [os.remove(os.path.join(PATH, f)) for f in os.listdir(PATH)]
 
     def test_renumber_alpha(self):
-        renumber = Renumber(path=TEST_PATH)
+        renumber = Renumber(path=RENUMBER_RELATIVE_PATH)
         # reorder test files
         renumber.renumber(sort='alpha')
         # get results
-        result = sorted(os.listdir())
+        result = sorted(os.listdir(PATH))
         expected_result = [
             '000_zoia_Afterneath_V4.bin',
             '001_zoia_Fading_Dream1_1.bin'
@@ -44,14 +39,14 @@ class TestRenumber(unittest.TestCase):
         # add some more files including dupes
         test_filenames = [
             '007_zoia_Afterneath_V4.bin',
-            '056_zoia_Fading_Dream1_1.bin'
+            '056_zoia_Fading_Dream1_1.bin',
             '003_zoia_some_other_file_name.bin',
             '004_zoia_file_name.bin'
         ]
-        [open(f'{TEST_PATH}{f}', 'a').close() for f in test_filenames]
-        renumber = Renumber(path=TEST_PATH)
+        [open(os.path.join(PATH, f), 'a').close() for f in test_filenames]
+        renumber = Renumber(path=RENUMBER_RELATIVE_PATH)
         renumber.renumber(sort='alpha')
-        result = sorted(os.listdir())
+        result = sorted(os.listdir(PATH))
         expected = [
             '000_zoia_Afterneath_V4.bin',
             '001_zoia_Afterneath_V4.bin',
@@ -63,11 +58,11 @@ class TestRenumber(unittest.TestCase):
         self.assertListEqual(result, expected)
 
     def test_renumber_random(self):
-        renumber = Renumber(path=TEST_PATH)
+        renumber = Renumber(path=RENUMBER_RELATIVE_PATH)
         # reorder test files
         renumber.renumber(sort='random')
         # get results
-        result = sorted(os.listdir())
+        result = sorted(os.listdir(PATH))
         # we can't assert that the results are truly random, but we can
         # at least make sure the numbering worked :)
         self.assertEqual('000', result[0].split('_')[0])
