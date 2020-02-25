96d6ce2c0b68ce524b4c176ebc7b378a9ee5105c
Mike Moger <>
2020-02-25 06:58:03 -0500

    Testing API downloads and endpoints using the 10 available on alpha

--- zoia_lib/api.py
+++ zoia_lib/api.py
@@ -5,10 +5,43 @@ Author: Mike Moger
 Usage: 
 """
 
+import os
 import requests
 
 
+class PatchStorage:
+
+    def __init__(self):
+        """initializes PatchStorage class"""
+
+        # set defaults for query params
+        self.url = 'https://patchstorage.com/api/alpha'
+        self.platform = 'zoia'
+
+    def endpoint(self,
+                 endpoint: str):
+        """specify endpoint for query"""
+
+        return os.path.join(self.url, endpoint)
+
+    def get_patch(self,
+                  **kwargs):
+        """get Patch object"""
+
+        patch = requests.get(url=self.endpoint('patches'),
+                             params=[('platform', self.platform)],
+                             **kwargs).json()
+
+        idx = str(patch.id)
+
+        mn = self.endpoint('patches')
+        dl = requests.get(url=os.path.join(mn, idx)).json().files.url
+
+        return dl
+
 # https://patchstorage.com/docs/
+
+
 def get_patch():
 
     base_url = 'https://patchstorage.com/api/alpha/patches/'
