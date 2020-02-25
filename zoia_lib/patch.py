6b9f64a6b09f168a6ed2473c7e5f621cf6594e0e
Mike Moger <>
2020-02-24 20:44:18 -0500

    Versioning and un-zip of packaged patches

--- zoia_lib/patch.py
+++ zoia_lib/patch.py
@@ -5,8 +5,10 @@ Author: Mike Moger
 Usage: 
 """
 
+import os
 import struct
 import zipfile
+from zoia_lib.common import errors
 
 
 class ZoiaPatch:
@@ -15,21 +17,60 @@ class ZoiaPatch:
                  filename: str):
         """Initializes ZoiaPatch class"""
 
+        self.path = os.path.dirname(filename)
+
         # Read single bin file
-        if filename.endswith('bin'):
-            data = open(filename, "rb").read()
-            self.name, self.number = struct.unpack('@II', data)
-            self.tag, self.version = struct.unpack('h1123b', data)
-            self.notes = self.patch_notes()
+        if filename.endswith('.bin'):
+            self.number, self.name = self.strip_header(filename)
+            """binary format requires specialized unpacking, waiting on Empress"""
+            # data = open(filename, 'rb').read()
+            # self.tag, self.version = struct.unpack('h1123b', data)
+            self.notes = self.patch_notes(self.name)
 
         # Read compressed dir
-        elif filename.endswith('zip'):
-            data = zipfile.ZipFile(filename, "rb")
-            for fls in data.filelist:
-                if fls.filename.endswith('bin'):
-                    self.name, self.number = fls
-                if fls.filename.endswith('txt'):
-                    self.notes = fls
+        elif filename.endswith('.zip'):
+            self.name = []
+            self.number = []
+            self.notes = []
+            data = zipfile.ZipFile(filename, "r")
+            for fl in data.namelist():
+                if fl.endswith('.bin'):
+                    self.number.append(self.strip_header(fl)[0])
+                    self.name.append(self.strip_header(fl)[1])
+                elif fl.endswith('.txt'):
+                    """read text file and add notes to object"""
+                    with data.open(fl) as f:
+                        for line in f:
+                            self.notes.append(line)
+                else:
+                    """create a note if the zip dir does not include one"""
+                    self.notes = self.patch_notes(self.name[0])
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, path):
+        if os.path.isdir(path):
+            self._path = os.path.abspath(path)
+            return
+        root_path = os.path.dirname(__file__)
+        _path = os.path.join(root_path, path)
+        if os.path.isdir(_path):
+            self._path = _path
+            return
+        else:
+            raise errors.BadPathError(
+                f'Supplied path {path} is not a child of '
+                f'current directory.'
+            )
+
+    @staticmethod
+    def strip_header(name: str):
+        """remove header ***_zoia_ from files"""
+
+        return name.split('_zoia_')[0], name[3:].split('_zoia_')[1].replace('.bin', '')
 
     def add_tags(self,
                  primary: list,
@@ -42,14 +83,19 @@ class ZoiaPatch:
                     to_remove: list):
         """Remove tags from Patch class"""
 
-        self.tag.remove([t for t in tag])
+        self.tag.remove([t for t in to_remove])
 
     def patch_notes(self,
+                    name: str,
                     action: str = 'add',
                     command: str = 'v 1.0'):
         """Add or edit patch notes"""
 
-        with open('{}_patch_notes.txt'.format(self.name), 'wb') as fl:
-            fl.write('{}'.format(command))
+        # options = {'add': self.add_notes,
+        #            'remove': self.remove_notes,
+        #            'update': self.update_notes}
+        if action == 'add':
+            with open('{}/{} patch notes.txt'.format(self.path, name), 'w') as fl:
+                fl.write('{}'.format(command))
 
         return fl
