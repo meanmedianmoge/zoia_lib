5f973ffdc46ed901143dd31cfd2ed0e26e0a6301
meanmedianmoge <mikebmoger@gmail.com>
2021-06-04 09:05:38 -0400

    Use different alias since numpy.unicode is deprecated

--- zoia_lib/backend/api.py
+++ zoia_lib/backend/api.py
@@ -7,7 +7,7 @@ import certifi
 import urllib3
 from bs4 import BeautifulSoup
 from furl import furl
-from numpy import unicode
+from numpy.compat import unicode
 
 http = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
 
