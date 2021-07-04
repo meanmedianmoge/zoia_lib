7f6ad3f7f732062321126b569e9f5a39b3347e43
Mike Moger <>
2021-07-04 15:57:56 -0400

    Patch expander experiments with NodeGraphQt

--- zoia_lib/UI/ZOIALibrarian_local.py
+++ zoia_lib/UI/ZOIALibrarian_local.py
@@ -2,8 +2,17 @@ import glob
 import json
 import os
 
+from NodeGraphQt import (
+    NodeGraph,
+    BaseNode,
+    BackdropNode,
+    PropertiesBinWidget,
+    NodeTreeWidget,
+    setup_context_menu
+)
+
+from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtGui import QIcon
-from PySide2 import QtCore
 from PySide2.QtCore import QEvent, QThread
 from PySide2.QtWidgets import (
     QMainWindow,
@@ -11,11 +20,11 @@ from PySide2.QtWidgets import (
     QInputDialog,
     QPushButton,
     QSpinBox,
-    QVBoxLayout,
+    # QVBoxLayout,
     QWidget,
 )
-from PySide2.QtWebEngineWidgets import QWebEngineView
-from pyvis.network import Network
+# from PySide2.QtWebEngineWidgets import QWebEngineView
+# from pyvis.network import Network
 
 from zoia_lib.backend.patch_update import PatchUpdate
 from zoia_lib.backend.utilities import hide_dotted_files
@@ -740,7 +749,24 @@ class ZOIALibrarianLocal(QMainWindow):
         viz: The parsed binary data that will be used in the visualizer.
         """
 
-        # Create a window
+        class MyNode(BaseNode):
+            """
+            example test node.
+            """
+
+            # set a unique node identifier.
+            __identifier__ = 'com.chantasticvfx'
+
+            # set the initial default node name.
+            NODE_NAME = 'my node'
+
+            def __init__(self):
+                super(MyNode, self).__init__()
+                self.set_color(25, 58, 51)
+
+        # QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+        # win = QtWidgets.QApplication([])
+
         win = QWidget()
         win.setWindowTitle(
             "Interactive Patch Routing: {}".format(self.curr_viz["name"])
@@ -750,91 +776,216 @@ class ZOIALibrarianLocal(QMainWindow):
                          "logo.ico")))
         win.setFixedSize(540, 540)
 
-        # And give it a layout
-        layout = QVBoxLayout()
-        win.setLayout(layout)
+        # create node graph.
+        graph = NodeGraph()
 
-        # Arbitrary storage of html
-        html_path = os.path.join(self.path, "patch.html")
+        # set up default menu and commands.
+        setup_context_menu(graph)
 
-        json_data = {
-            "nodes": self.curr_viz["modules"],
-            "links": self.curr_viz["connections"],
-        }
+        # widget used for the node graph.
+        graph_widget = graph.widget
+        graph_widget.resize(1100, 800)
+        graph_widget.show()
 
-        mods = [
-            (
-                m["number"],
-                m["type"],
-                m["category"],
-                m["name"] if m["name"] != "" else m["type"],
-            )
-            for m in json_data["nodes"]
+        # show the properties bin when a node is "double clicked" in the graph.
+        properties_bin = PropertiesBinWidget(node_graph=graph)
+        properties_bin.setWindowFlags(QtCore.Qt.Tool)
+
+        def show_prop_bin(node):
+            if not properties_bin.isVisible():
+                properties_bin.show()
+
+        graph.node_double_clicked.connect(show_prop_bin)
+
+        # show the nodes list when a node is "double clicked" in the graph.
+        node_tree = NodeTreeWidget(node_graph=graph)
+
+        def show_nodes_list(node):
+            if not node_tree.isVisible():
+                node_tree.update()
+                node_tree.show()
+
+        graph.node_double_clicked.connect(show_nodes_list)
+
+        # registered nodes.
+        nodes_to_reg = [
+            # BackdropNode,
+            MyNode,
+            # basic_nodes.FooNode,
+            # basic_nodes.BarNode,
+            # widget_nodes.DropdownMenuNode,
+            # widget_nodes.TextInputNode,
+            # widget_nodes.CheckboxNode
         ]
+        graph.register_nodes(nodes_to_reg)
 
-        conns = [
-            (
-                int(c["source"].split(".")[0]),
-                int(c["destination"].split(".")[0]),
-                c["strength"] / 100,
+        pch = self.curr_viz
+
+        nodes = {}
+        for module in pch['modules']:
+            my_node = graph.create_node(
+                'com.chantasticvfx.MyNode',
+                name=(module['type'] if module['name'] == '' else module['name']) + str(module["number"]),
+                color='#0a1e20',
+                text_color='#feab20'
             )
-            for c in json_data["links"]
-        ]
+            inp, outp, in_pos, out_pos = [], [], [], []
+            for key, param in module['blocks'].items():
+                if 'in' in key:
+                    my_node.add_input(key)
+                    inp.append(key)
+                    in_pos.append(int(param["position"]))
+                elif param["isParam"]:
+                    my_node.add_input(key)
+                    inp.append(key)
+                    in_pos.append(int(param["position"]))
+                elif 'out' in key:
+                    my_node.add_output(key)
+                    outp.append(key)
+                    out_pos.append(int(param["position"]))
+            nodes[module["number"]] = my_node, inp, outp, in_pos, out_pos
 
-        nt = Network()
-        # nx_graph = nx.DiGraph()
+        # print(nodes)
 
-        for m in mods:
-            nt.add_node(m[0], size=20, title=m[1], group=m[2], label=m[3])
+        # map pos from nodes to connections
+        def node_pos_map(node):
+            inpts = node[1]
+            outps = node[2]
+            in_pos = node[3]
+            out_pos = node[4]
+            node_pos_start = [x for x in range(0, len(inpts))]
+            node_pos_end = [x for x in range(0, len(outps))]
+            data_input = dict(zip(in_pos, node_pos_start))
+            data_output = dict(zip(out_pos, node_pos_end))
 
-        for c in conns:
-            nt.add_edge(c[0], c[1], weight=c[2])
+            return {**data_input, **data_output}
 
-        # nt.from_nx(nx_graph)
-        # nt.show_buttons()
+        data = []
+        for key, node in nodes.items():
+            data.append(node_pos_map(node))
 
-        nt.set_options(
-            """
-        var_options = {
-            "edges": {
-                "arrows": {
-                    "middle": {
-                        "enabled": true
-                    }
-                },
-                "color": {
-                    "inherit": true
-                },
-                "smooth": false
-            },
-            "interaction": {
-                "navigationButtons": true
-            },
-            "manipulation": {
-                "enabled": true
-            },
-            "physics": {
-                "minVelocity": 0.75
-            }
-        }
-        """
-        )
+        for conn in pch['connections']:
+            mod, block = conn['source'].split('.')
+            nmod, nblock = conn['destination'].split('.')
+            src = data[int(mod)]
+            dest = data[int(nmod)]
+            try:
+                nodes[int(mod)][0].set_output(
+                    src[int(block)],
+                    nodes[int(nmod)][0].input(dest[int(nblock)])
+                )
+            except KeyError as e:
+                print(conn, e)
+            except IndexError as e:
+                print(conn, e)
+        print('done making connections')
 
-        nt.show(html_path)
+        # auto layout nodes.
+        graph.auto_layout_nodes()
 
-        with open(html_path, "r") as f:
-            grph = f.read()
+        # wrap a backdrop node.
+        # backdrop_node = graph.create_node('nodeGraphQt.nodes.BackdropNode')
+        # backdrop_node.wrap_nodes([text_node, checkbox_node])
 
-        # Create and fill a QWebView
-        view = QWebEngineView()
-        view.setHtml(grph)
+        graph.fit_to_selection()
 
-        # Add the QWebView and button to the layout
-        layout.addWidget(view)
-
-        # Show the window and run the app
-        win.show()
         win.exec_()
+
+        # # Create a window
+        # win = QWidget()
+        # win.setWindowTitle(
+        #     "Interactive Patch Routing: {}".format(self.curr_viz["name"])
+        # )
+        # win.setWindowIcon(QIcon(
+        #     os.path.join(os.getcwd(), "zoia_lib", "UI", "resources",
+        #                  "logo.ico")))
+        # win.setFixedSize(540, 540)
+        #
+        # # And give it a layout
+        # layout = QVBoxLayout()
+        # win.setLayout(layout)
+        #
+        # # Arbitrary storage of html
+        # html_path = os.path.join(self.path, "patch.html")
+        #
+        # json_data = {
+        #     "nodes": self.curr_viz["modules"],
+        #     "links": self.curr_viz["connections"],
+        # }
+        #
+        # mods = [
+        #     (
+        #         m["number"],
+        #         m["type"],
+        #         m["category"],
+        #         m["name"] if m["name"] != "" else m["type"],
+        #     )
+        #     for m in json_data["nodes"]
+        # ]
+        #
+        # conns = [
+        #     (
+        #         int(c["source"].split(".")[0]),
+        #         int(c["destination"].split(".")[0]),
+        #         c["strength"] / 100,
+        #     )
+        #     for c in json_data["links"]
+        # ]
+        #
+        # nt = Network()
+        # # nx_graph = nx.DiGraph()
+        #
+        # for m in mods:
+        #     nt.add_node(m[0], size=20, title=m[1], group=m[2], label=m[3])
+        #
+        # for c in conns:
+        #     nt.add_edge(c[0], c[1], weight=c[2])
+        #
+        # # nt.from_nx(nx_graph)
+        # # nt.show_buttons()
+        #
+        # nt.set_options(
+        #     """
+        # var_options = {
+        #     "edges": {
+        #         "arrows": {
+        #             "middle": {
+        #                 "enabled": true
+        #             }
+        #         },
+        #         "color": {
+        #             "inherit": true
+        #         },
+        #         "smooth": false
+        #     },
+        #     "interaction": {
+        #         "navigationButtons": true
+        #     },
+        #     "manipulation": {
+        #         "enabled": true
+        #     },
+        #     "physics": {
+        #         "minVelocity": 0.75
+        #     }
+        # }
+        # """
+        # )
+        #
+        # nt.show(html_path)
+        #
+        # with open(html_path, "r") as f:
+        #     grph = f.read()
+        #
+        # # Create and fill a QWebView
+        # view = QWebEngineView()
+        # view.setHtml(grph)
+        #
+        # # Add the QWebView and button to the layout
+        # layout.addWidget(view)
+        #
+        # # Show the window and run the app
+        # win.show()
+        # win.exec_()
 
     def viz_page(self):
         """Either increases or decreases the current visualizer page,
