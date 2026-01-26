4c53a9fe7e3bcfae8cd02a16be1da05ebf65f188
meanmedianmoge <mikebmoger@gmail.com>
2026-01-26 11:23:58 -0500

    Third time's the charm

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,9 @@
 ARCHITECTURE="${1:-$(uname -m)}"
 if [ "$ARCHITECTURE" = "x86_64" ]; then
-    python -m venv venv_x86
+    python3 -m venv venv_x86
     source venv_x86/bin/activate
 else
-    python -m venv venv
+    python3 -m venv venv
     source venv/bin/activate
 fi
 
